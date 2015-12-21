8470274275a14d9eaad85aca36d16665bbf923c8
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2015-12-21 10:26:12 +0700

    update

---
+++ simpleBlog/server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var express = require('express'),
+		path    = require('path'),
+		Bourne  = require("bourne"),
+		app = express(),
+		posts = new Bourne("simpleBlogPosts.json"),
+		comments = new Bourne("simpleBlogComments.json")
+	;
+
+app.configure(function() {
+	app.use(express.json());
+	app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.get('/*', function(req, res) {
+	res.render("index.ejs");
+});
+
+app.listen(3000);
